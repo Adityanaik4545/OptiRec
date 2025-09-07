3dce4f77531f3f74b33df534259654ea6b24047a
Adityanaik4545 <adityank23@outlook.com>
2025-09-07 19:50:05 +0530

    done with navigationBar and header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
+    <main className='page wrapper' >
+      <Header title='All Videos' subHeader='Public library' />
       <h1 className=' font-karla text-2xl '>Welcom to screen recorder</h1>
-    </div>
+    </main>
   )
 }
 
