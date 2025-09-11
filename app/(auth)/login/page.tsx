1a55ac25540b5a96becfa01db39b7c928f16dd45
Adityanaik4545 <adityank23@outlook.com>
2025-09-11 19:38:59 +0530

    created handle sign in  function for sign button

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,15 @@
+"use client"
+import { authClient } from '@/lib/auth-client'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
+
+  const handleSignIn = async() =>{
+    return await authClient.signIn.social({provider: 'google'})
+  }
+
   return (
     <main className='sign-in' >
       <aside className='testimonial' >
@@ -39,7 +46,7 @@ const page = () => {
           <h1>OptiRec</h1>
           </Link>
           <p>Create and share your very first <span>OptiRec video</span> in no time</p>
-          <button>
+          <button onClick={handleSignIn} >
             <Image src="/assets/icons/google.svg" width={22} height={22} alt='google' />
             <span>Sign in with Google</span>
           </button>
