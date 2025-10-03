52a5c19c500c8e6ea849ab54eeb4de5ea5f4c627
Adityanaik4545 <adityank23@outlook.com>
2025-10-03 23:53:23 +0530

    added video processing status tracking and auto-increment view count in player

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,10 +1,58 @@
+"use client"
+import { initialVideoState } from '@/constants'
 import { videos } from '@/drizzle/schema'
-import { createIframeLink } from '@/lib/utils'
-import React from 'react'
+import { getVideoProcessingStatus, increamentVideoViews } from '@/lib/actions/video'
+import { cn, createIframeLink } from '@/lib/utils'
+import React, { useEffect, useRef, useState } from 'react'
 
-const Player = ({videoId}:VideoPlayerProps) => {
+const Player = ({videoId, className}:VideoPlayerProps) => {
+  const iframeRef = useRef<HTMLIFrameElement>(null)
+  const [state, setState] = useState(initialVideoState);
+
+  useEffect(()=>{
+    const checkProcessingStatus = async() =>{
+    const status = await getVideoProcessingStatus(videoId);
+    setState((prev)=>({
+      ...prev,
+      isProcessing: !status.isProcessed,
+    }));
+
+    return status.isProcessed;
+    };
+
+    checkProcessingStatus();
+
+    const intervalId = setInterval(async() => {
+      const isProcessed = await checkProcessingStatus();
+      if (isProcessed) {
+        clearInterval(intervalId);
+      }
+    }, 3000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  },[videoId])
+
+  useEffect(()=>{
+    if(state.isLoaded && !state.hasIncrementedView && !state.isProcessing){
+      const increamentView = async() =>{
+        try {
+          await increamentVideoViews(videoId);
+          setState((pre)=>({...pre, hasIncrementedView:true}));
+        } catch (error) {
+          console.error("Failed to increament view count:",error);
+        }
+      };
+      increamentView();
+    }
+  },[videoId, state.hasIncrementedView, state.isLoaded, state.isProcessing]);
   return (
-    <div className='video-player' >
+    <div className={cn('video-player', className)} >
+      {state.isProcessing ?(
+        <div>
+          <p>Processing video...</p>
+        </div>
+      ):(
       <iframe
             src={createIframeLink((videoId))}
             loading='lazy'
@@ -12,7 +60,9 @@ const Player = ({videoId}:VideoPlayerProps) => {
             style={{border:0, zIndex:50}}
             allowFullScreen
             allow='accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture'
+            onLoad={()=>setState((prev)=>({...prev, isLoaded:true}))}
       />
+      )}
     </div>
   )
 }
