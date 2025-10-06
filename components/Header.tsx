85ba19962cee5f2c4db7ed66f29fa3773de53a3f
Adityanaik4545 <adityank23@outlook.com>
2025-10-06 21:19:34 +0530

    implemented video filter functionality to allow  users to sort videos

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,73 @@
-import { ICONS } from '@/constants'
+"use client"
+import { filterOptions, ICONS } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DropdownList from './DropdownList'
 import RecordScreen from './RecordScreen'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { updateURLParams } from '@/lib/utils'
 
 const Header = ({subHeader, title, userImg}:SharedHeaderProps) => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParmas = useSearchParams();
+
+  const [searchQuery, setSearchQuery] =useState(
+    searchParmas.get("query") || ""
+  );
+  const [selectedFilter, setSelectedFilter] =useState(
+    searchParmas.get("filter") || "Most Recent"
+  );
+
+  useEffect(()=>{
+    setSearchQuery(searchParmas.get("query") || "");
+    setSelectedFilter(searchParmas.get("filter") || "Most Recent");
+  },[searchParmas]);
+
+  useEffect(()=>{
+    const debounceTimer = setTimeout(() => {
+      if (searchQuery !== searchParmas.get("query")) {
+        const url = updateURLParams(
+          searchParmas,
+          {query: searchQuery || null},
+          pathname
+        )
+        router.push(url);
+      }
+      
+    }, 1000);
+    return () => clearTimeout(debounceTimer)
+  },[searchQuery,searchParmas,pathname,router])
+
+  const handlleFilterChange = (filter: string) =>{
+    setSelectedFilter(filter)
+    const url = updateURLParams(
+      searchParmas,
+      {filter: filter || null},
+      pathname
+    );
+    router.push(url);
+  };
+
+  const renderFilterTrigger = (
+    <div  className='filter-trigger'>
+      <figure>
+        <Image src="/assets/icons/hamburger.svg" 
+        alt='hamburger' 
+        width={14} 
+        height={14} 
+        />
+        <span>{selectedFilter}</span>
+      </figure>
+      <Image src="/assets/icons/arrow-down.svg" 
+      alt='arrow-down' 
+      width={20} 
+      height={20} 
+      />
+    </div>
+  )
+
   return (
     <header className='header' >
       <section className='header-container' >
@@ -30,10 +92,24 @@ const Header = ({subHeader, title, userImg}:SharedHeaderProps) => {
 
       <section className='search-filter' >
             <div className='search' >
-                <input type="text" placeholder='Search for videos, tags, folders' />
-                <Image src="/assets/icons/search.svg" width={16} height={16} alt='search' />
+                <input 
+                type="text" 
+                placeholder='Search for videos, tags, folders'
+                value={searchQuery}
+                onChange={(e)=>setSearchQuery(e.target.value)}
+                />
+                <Image src="/assets/icons/search.svg" 
+                width={16} 
+                height={16} 
+                alt='search'
+                />
             </div>
-            <DropdownList/>
+            <DropdownList
+            options={filterOptions}
+            selectedOption={selectedFilter}
+            onOptionSelect={handlleFilterChange}
+            triggerElement={renderFilterTrigger}
+            />
       </section>
     </header>
   )
