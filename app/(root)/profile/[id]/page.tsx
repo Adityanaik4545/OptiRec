55b30cb0c778e74c276d016aadcb1969a6822fe8
Adityanaik4545 <adityank23@outlook.com>
2025-10-06 21:15:52 +0530

    fix: updated videocard prop because it was routing to wrong video id

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,10 +1,9 @@
 import EmptyState from '@/components/EmptyState';
 import Header from '@/components/Header';
 import VideoCard from '@/components/VideoCard';
-import { dummyCards } from '@/constants';
 import { getAllVideosByUser } from '@/lib/actions/video';
 import { redirect } from 'next/navigation';
-import React, { use } from 'react'
+import React from 'react'
 
 const page = async({params, searchParams}:ParamsWithSearch) => {
     const {id} = await params;
@@ -14,25 +13,36 @@ const page = async({params, searchParams}:ParamsWithSearch) => {
 
     if(!user) redirect("/404");
   return (
-    <div className='wrapper page' >
-    <Header subHeader={user?.email} title={user?.name} userImg={user?.image ?? ''} />
-{videos?.length > 0 ?
-              (
-                  <section className='video-grid' >
-                      {videos.map(({video, user})=>(
-                          <VideoCard 
-                          key={video.id} 
-                          {...video} 
-                          thumbnail={video.thumbnailUrl} 
-                          userImg={user?.image || ''}
-                          username={user?.name || 'Guest'}
-                           />
-                      ))}
-                  </section>
-              ):(
-                  <EmptyState icon='/assets/icons/video.svg' title='No Videos Available Yet' description='Videos will show up once you upload them' />
-              )}
-    </div>
+    <main className='wrapper page' >
+    <Header 
+    subHeader={user?.email} 
+    title={user?.name} 
+    userImg={user?.image ?? ''} 
+    />
+    {videos?.length > 0 ? (
+        <section className='video-grid' >
+            {videos.map(({video})=>(
+                <VideoCard 
+                key={video.id}
+                id={video.videoId}
+                title={video.title}
+                thumbnail={video.thumbnailUrl}
+                createdAt={video.createdAt}
+                userImg={user.image ?? ""}
+                username={user.name ?? "Guest"}
+                views={video.views}
+                visibility={video.visibility}
+                duration={video.duration}
+            />
+        ))}
+        </section>
+    ):(
+        <EmptyState icon='/assets/icons/video.svg' 
+        title='No Videos Available Yet' 
+        description='Videos will show up once you upload them' 
+        />
+    )}
+    </main>
   )
 }
 
